2cbdf119e8e411282a30400f028b805d40bb1632
Rafael Viegas <rafael.viegas@arpagus.com.br>
2016-12-08 14:36:53 -0200

    3. Lembrando do ensino fundamental

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -4,4 +4,13 @@ var app = express();
 //configurações do express
 app.use(express.static('./public'));
 
+app.get('/v1/fotos', function(req, res){
+    var fotos = [
+        {_id:1,titulo:'Leão',url:'http://www.fundosanimais.com/Minis/leoes.jpg'},
+        {_id:2,titulo:'Leão 2',url:'http://www.fundosanimais.com/Minis/leoes.jpg'}
+    ];
+
+    res.json(fotos);
+});
+
 module.exports = app;
